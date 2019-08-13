1a2f5a69f2d7ebb9fc60d7d50999588a4ec9db71
Rodrigo <roizpi@users.noreply.github.com>
2019-08-13 23:34:58 +0200

    Extended example

--- 2. rxjx-lib/operators2.ts
+++ 2. rxjx-lib/operators2.ts
@@ -9,3 +9,53 @@ const squareOdds = new Observable.of(1, 2, 3, 4, 5).pipe(
 
 // subscribe to trigger the sequence.
 squareOdds.subscribe(x => console.log(x));
+
+
+// merge two flows
+const flowX = new Observable((obs) => {
+    const values = [1, 2, 3, 4, 5, 6];
+    let timeoutId;
+    function doSequence(list, index) {
+        timeoutId = setTimeout(() => {
+            obs.next(list[index]);
+            if (index === list.length -1) {
+                obs.complete();
+            } else {
+                doSequence(list, index + 1);
+            }
+        }, 500);
+    }
+    doSequence(values, 0);
+
+    return {
+        unsubscribe() {
+            clearTimeout(timeoutId);
+        }
+    }
+});
+
+const flowY = new Observable((obs) => {
+    const values = ['a', 'b', 'c'];
+    let timeoutId;
+    function doSequence(list, index) {
+        timeoutId = setTimeout(() => {
+            obs.next(list[index]);
+            if ( index === list.length -1) {
+                obs.complete();
+            } else {
+                doSequence(list, index + 1);
+            }
+        }, 1000);
+    }
+    doSequence(values, 0);
+
+    return {
+        unsubscribe() {
+            clearTimeout(timeoutId);
+        }
+    }
+});
+
+// outputs: 1,a,2,3,b,4,5,c,6
+let observableMerge = flowX.merge(flowY);
+observableMerge.subscribe(x => console.log(x));
